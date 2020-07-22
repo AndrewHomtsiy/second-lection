8ce4e8ab7bd2b764e72f76a0fcc2a329d1167e86
AndrewHomtsiy <andriyko1395@gmail.com>
2020-07-22 22:23:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-let firstNumber, secondNumber ='';
 let sum = 0;
-
-do {
-  firstNumber = +prompt("Введіть 1 число:");
-  if (Number.isInteger(firstNumber) && firstNumber !== '') {
-    continue;
-  }
-  alert ("Введіть будь ласка ціле 1 число:");
-} while (!Number.isInteger(firstNumber) &&firstNumber !== 0);
-
-
-do {
-  secondNumber = +prompt("Введіть 2 число:");
-  if (Number.isInteger(secondNumber) && secondNumber !== '') {
-    continue;
-  }
- alert ("Введіть будь ласка ціле 2 число:");
-} while (!Number.isInteger(secondNumber) && firstNumber !== 0);
-
-
+let firstNumber  = +prompt("Введіть 1 число:");
+while(!Number.isInteger(firstNumber) || firstNumber == '') {
+    alert("Введіть будь ласка ціле 1 число:")
+    firstNumber = +prompt("Введіть 1 число:");
+}
+let secondNumber = +prompt ("Введіть будь ласка 2 число");
+while(!Number.isInteger(secondNumber) || secondNumber == '') {
+    alert("Введіть будь ласка ціле 2 число:")
+    secondNumber = +prompt("Введіть 2 число:");
+}
 const missNum = confirm("Пропускати парні числа?");
 
 console.log(
 `Число 1: ${firstNumber};
 Число 2: ${secondNumber};
 Пропускати парні числа?: ${missNum}`);
+
+
 
 const max = Math.max(firstNumber, secondNumber);
 const min = Math.min(firstNumber, secondNumber);
@@ -38,8 +30,4 @@ const min = Math.min(firstNumber, secondNumber);
     }
 
 console.log(`Сума чисел: ${sum}`);
-
-
-
-
 
